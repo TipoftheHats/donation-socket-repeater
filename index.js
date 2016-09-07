773c530d4cfeecc734086442635c5336cd662c67
Alex Van Camp <vancamp.alex@gmail.com>
2016-09-07 14:36:51 -0500

    lol

--- index.js
+++ index.js
@@ -4,9 +4,6 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-
-server.listen(80);
-
 const convict = require('convict');
 const conf = convict({
 	port: {
@@ -25,7 +22,7 @@ const conf = convict({
 	}
 }).getProperties();
 
-app.listen(conf.port);
+server.listen(conf.port);
 console.log(`Listening on port ${conf.port}.`);
 
 app.get('/', (req, res) => {
